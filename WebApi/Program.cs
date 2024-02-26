c2308149d9c59dee438c861430cec025294d2eaf
Omer99 <oyildirim3510@gmail.com>
2024-02-26 17:22:53 +0300

    Day 3 Last Commit

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,9 @@
+using Business.Abstracts;
+using Business.Concretes;
+using DataAccess.Abstracts;
+using DataAccess.Concretes;
+using Entities.Concretes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IBrandService, BrandManager>();
+builder.Services.AddSingleton<IBrandDal, BrandDal>();
 
 var app = builder.Build();
 
